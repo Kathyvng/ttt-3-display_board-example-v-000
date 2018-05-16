ac33cf7a431883d1f0ddcaea3bc894e6c905baac
Kathy Nguyen <kathyvng@gmail.com>
2018-05-16 03:19:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,4 @@ def display_board
   print "-----------\n"
   print "   |   |   \n"
 end
-print "   |   |   \n"
-print "-----------\n"
-print "   |   |   \n"
-print "-----------\n"
-print "   |   |   \n"
+
